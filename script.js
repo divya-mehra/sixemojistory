a5eac70ef0e529ecf00199d72444b68447dc561b
divya-mehra <divyahmehra@gmail.com>
2023-07-31 23:47:09 -0400

    grid

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-let options = {
-    // root: document.querySelector("#scrollArea"),
-    // rootMargin: "100% 0% 100% 0%",
-    threshold: 0.9,
-  };
+// make grid
 
-  const handleIntersection = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(entry.target.id)
-        console.log("Element with ID " + entry.target.id + " is intersecting!");
-      }
-    });
-  };
-  
+const fragment = document.createDocumentFragment();
+
+for (let i = 1; i <= 144; i++) {
+  let rect = document.createElement("div");
+  let divId = "rect-" + i;
+  rect.classList.add("rect");
+  rect.setAttribute("id", divId);
+  fragment.append(rect);
+}
+
+const gridPanels = document.querySelector(".gridPanel");
+
+gridPanels.append(fragment);
+
+// intersection observer
+
+let options = {
+  // root: document.querySelector("#scrollArea"),
+  // rootMargin: "100% 0% 100% 0%",
+  threshold: 0.9,
+};
+
+const handleIntersection = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry.target.id);
+      console.log("Element with ID " + entry.target.id + " is intersecting!");
+    }
+  });
+};
+
 let observer = new IntersectionObserver(handleIntersection, options);
 
 let panels = document.querySelectorAll(".textPanel");
 
-console.log(panels)
+console.log(panels);
 
 panels.forEach((p) => {
-    observer.observe(p);
-    
-})
-
-
+  observer.observe(p);
+});
