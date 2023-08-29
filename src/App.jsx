676c2a279027e636b1f00b9181ef4cbb8980da2e
divya-mehra <divyahmehra@gmail.com>
2023-08-28 23:45:07 -0400

    changes to subtitles

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,32 @@ function App() {
       number: "01",
       title: "Young ♥️",
       content: ["hi"],
-      subtitle: "1997: It's always about love",
+      subtitle: "1999: The second* set of emojis is created in Japan",
     },
     {
       name: "poo",
       number: "02",
       title: "You're a Pile of 💩",
-      subtitle: "2007: Google pays attention emojis when expanding to global markets",
+      subtitle: "2007: Google considers designing for international markets",
     },
     {
       name: "hand",
       number: "03",
       title: "✊🏻✊🏼✊🏽✊🏾✊🏿 ~ Department of DEI ~",
       subtitle:
-        "2015: Skin tone modifiers move the emoji from symbolic toward representative",
+        "2015: Unicode adds five skin tone modifiers to its standard yellow",
     },
     {
       name: "bee",
       number: "04",
       title: "Becky! Leave Queen 🐝 Alone 🍋",
-      subtitle: "2016: And with that, each additional emoji invited questions",
+      subtitle: "2016: Pop star Beyonce's release of Lemonade leads to a misunderstanding on chef Rachel Ray's Instagram",
     },
     {
       name: "pistol",
       number: "05",
       title: " Does 🔫 breed real 🔫?",
-      subtitle: "2018: And with that, each additional emoji invited questions",
+      subtitle: "2018: Apple replaces the gun with a water pistol",
       
       
     },
@@ -48,7 +48,7 @@ function App() {
       name: "thumbsup",
       number: "06",
       title: "👍 (I'll see you in court)",
-      subtitle: "2023: And with that, each additional emoji invited questions",
+      subtitle: "2023: A Canadian judge rules that the thumbs up emoji can be considered a contractual signature",
     },
   ];
 
@@ -61,8 +61,8 @@ function App() {
 
   return (
     <>
-      {/* <div className="contentWrapper textLight"> */}
-      <div className="contentWrapper">
+      <div className="contentWrapper textLight">
+      {/* <div className="contentWrapper"> */}
         <SideStickyNav />
         <div className="leftSide">
           <div id="LearnStory">
