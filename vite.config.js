dc3025e1a3abd6e45a4f74a2fb05fa8a5c612452
divya-mehra <divyahmehra@gmail.com>
2023-09-02 16:35:14 -0400

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/sixemojistory/",
-  base: "/emoji.divya-mehra.com/",
+  base: "/sixemojistory/",
+  // base: "/emoji.divya-mehra.com/",
+  // base: "/",
   plugins: [react()],
 })
