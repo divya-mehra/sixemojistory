5acac5ccc35cfe35d57860a72f27e79596d3f0e2
divya-mehra <divyahmehra@gmail.com>
2023-09-02 16:22:05 -0400

    base change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/sixemojistory/",
-  base: "emoji.divya-mehra.com/",
+  base: "/emoji.divya-mehra.com/",
   plugins: [react()],
 })
